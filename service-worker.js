a1ee4758b6164e05e66c0b9a14c946e6ca9111a7
Serhii Antonyshyn <serginio4422@gmail.com>
2020-09-14 18:57:30 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.575c894ccaaaec018efa3e6d1c5de3fa.js"
+  "./precache-manifest.a203abd771cbe381f32a5e8dab30e680.js"
 );
 
 self.addEventListener('message', (event) => {
